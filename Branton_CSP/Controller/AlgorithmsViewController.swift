6b1eb0bb5b2dd27f70f5e14256c408dafbe372e8
BraisleCoanel <31518773+BraisleCoanel@users.noreply.github.com>
2018-01-16 13:52:33 -0700

    Made Algorithms storyboard

--- Branton_CSP/Controller/AlgorithmsViewController.swift
+++ Branton_CSP/Controller/AlgorithmsViewController.swift
@@ -8,15 +8,20 @@
 
 import UIKit
 
-class AlgorithmsViewController: UIViewController {
+class AlgorithmsViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet var ImageJava: UIImageView!
+    @IBOutlet var ImageSwift: UIImageView!
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
